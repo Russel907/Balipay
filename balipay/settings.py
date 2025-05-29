736ec52ca42da1368eaab7ccc598bbb24f8d505b
fairoosa <fairoosa02112000@gmail.com>
2025-05-29 17:25:03 +0530

    added requirement.txt file

--- balipay/settings.py
+++ balipay/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = "django-insecure-08p(51c8gzsk@$f3o&)h0nly$7idk8wu4^p_zypl7p(2251z0b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['razal.pythonanywhere.com']
+
 
 
 # Application definition
@@ -121,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
 
 
 # Default primary key field type
